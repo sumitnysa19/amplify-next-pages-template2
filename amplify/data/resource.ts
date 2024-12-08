7effbc1f90e83e0341b650e11729b4f44f163d92
Sumit <sumitnysa19@gmail.com>
2024-12-08 03:10:36 -0800

    Add external data source

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,7 +12,19 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+
+  Post: a.customType({
+    id: a.id().required(),
+    author: a.string().required(),
+    title: a.string(),
+    content: a.string(),
+    url: a.string(),
+    ups: a.integer(),
+    downs: a.integer(),
+    version: a.integer()
+  }),
 });
+
 
 export type Schema = ClientSchema<typeof schema>;
 
